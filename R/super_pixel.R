f2aee8dd9a2e1fce99a35b969b71fde5b869c633
patrickCNMartin <pcnmartin@gmail.com>
2023-01-05 15:33:21 -0800

    Feature(super pixel): truing to get center pixel init to work. Going to be slow in current form

--- R/super_pixel.R
+++ R/super_pixel.R
@@ -7,10 +7,8 @@
 #' select inital indices
 #' @param coordinates data frame containing spatial coordinates
 #' @param k numeric - number of intial coordinates
-#' @param random logial defining if initial starting points should be
-#' selected at random - if false - approximate number
 #' @return barcodes of starting coordinates
-#' importFrom future.apply, future_lapply
+#' @importFrom future.apply future_apply
 select_initial_indices <- function(coordinates,
     k = 500) {
     #---------------------------------------------------------------------#
@@ -21,32 +19,37 @@ select_initial_indices <- function(coordinates,
         max(coordinates$x),
         length.out = (2 * floor(box_size)) - 1)
     x <- x[seq(2, length(x), by = 2)]
-    x2 <- mean(tail(x, length(x) - 1) - head(x, length(x) - 1))
+   
     y <- seq(min(coordinates$y),
         max(coordinates$y),
         length.out = (2 * floor(box_size)) - 1)
     y <- y[seq(2, length(y), by = 2)]
-    y2 <- mean(tail(y, length(y) - 1) - head(y, length(y) - 1))
     grid <- cbind(rep(x, each = length(y)),
-        rep(y, times = length(x)))
-    grid <- future_apply(grid,
+        rep(y, times = length(x)),
+        seq(1, length(y) * length(x)))
+    grid <- future_apply(coordinates,
         MARGIN = 1,
         closet_grid_point,
-        coordinates,
-        x2,
-        y2)
-    indices <- do.call("rbind", grid)
-    return(indices)
+        grid)
+    
+    grid <- do.call("rbind", grid)
+    return(grid)
 }
 
 #' closest grid point
 #' @param grid grid matrix containing coordinate paiur of grid point centers
 #' @param coordinates data frame with spatial indices
 #' @return closest coordinates to grid mid point
-closet_grid_point <- function(grid, coordinates, x2, y2) {
-    dist <- sqrt(((coordinates$x - grid[1])^2 + (coordinates$y - grid[2])^2))
-    coord <- coordinates[order(dist, decreasing = FALSE), ]
-    coord$dist <- dist
-    return(coord)
+closet_grid_point <- function(coordinates, grid) {
+    distance <- sqrt(((grid[, 1] + as.numeric(coordinates[2]))^2 +
+        (grid[, 2] + as.numeric(coordinates[3]))^2))
+    distance <- which(distance == min(distance))
+    coordinates <- data.frame("barcodes" = coordinates[1],
+        "xcoord" = coordinates[2],
+        "ycoord" = coordinates[3],
+        "x_cent" = grid[distance, 1],
+        "y_cent" = grid[distance, 2],
+        "center" = grid[distance, 3])
+    return(coordinates)
 }
 
