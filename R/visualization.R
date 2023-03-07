6f5ebc12cf66a44537a34ccaad548463b7ae4446
patrickCNMartin <pcnmartin@gmail.com>
2023-03-07 15:52:58 -0800

    Feature(view gene expression): added background and transparency. Increases clarity for lowly expressd genes

--- R/visualization.R
+++ R/visualization.R
@@ -341,7 +341,9 @@ create_alpha <- function(territories, highlight) {
 #' @param norm logical indicating if expression should be min/max normalised
 #' @param as_layer logical indicating if expression should represented as
 #' a territory/ layer.
-#' @param cex numeric - font/point size modulator
+#' @param cex numeric - font size modulator
+#' @param cex_pt numeric point size
+#' @param alpha point transparency
 #' @details Vesalius offers a plotting function that allows you to 
 #' visualise gene expression. 
 #' 
@@ -421,7 +423,10 @@ view_gene_expression <- function(vesalius_assay,
   cells = NULL,
   norm = TRUE,
   as_layer = FALSE,
-  cex = 10) {
+  with_background = FALSE,
+  cex = 10,
+  cex_pt = 1,
+  alpha = 0.75) {
     #--------------------------------------------------------------------------#
     # First lets get the norm method out and the associated counts
     #--------------------------------------------------------------------------#
@@ -445,9 +450,10 @@ view_gene_expression <- function(vesalius_assay,
     for (i in seq_along(genes)) {
       gene <- rownames(counts) == genes[i]
       if (sum(gene) == 0) {
-        stop(paste(genes[i], "is not present in count matrix.
+        warning(paste(genes[i], "is not present in count matrix.
           Returning NULL"), immediate. = TRUE)
-        
+        gene_list[[i]] <- NULL
+        next()
       }
       gene <- counts[gene, ]
       gene <- data.frame(names(gene), gene)
@@ -463,10 +469,10 @@ view_gene_expression <- function(vesalius_assay,
       #------------------------------------------------------------------------#
       # Extract data and convert count to mean count if as_layer=T
       #------------------------------------------------------------------------#
+      if (is.null(gene_list[[i]])) next()
+
       other <- filter(gene_list[[i]], trial == "other") %>% droplevels()
       territory <- filter(gene_list[[i]], trial != "other") %>% droplevels()
-
-      
       if (as_layer) {
           territory <- territory %>%
             group_by(trial) %>%
@@ -488,9 +494,9 @@ view_gene_expression <- function(vesalius_assay,
       if (nrow(other) > 0) {
         ge <- ggplot() +
           geom_point(data = other, aes(x, y),
-            alpha = 0.75,
+            alpha = alpha,
             fill = "#2e2e2e",
-            size = cex * 0.01,
+            size = cex_pt,
             show.legend = TRUE)
       } else {
         ge <- ggplot()
@@ -500,27 +506,39 @@ view_gene_expression <- function(vesalius_assay,
       #------------------------------------------------------------------------#
       if (is.null(cells)) {
         ge <- ge + geom_point(data = territory,
-          aes(x = x, y = y, col = gene), size = cex * 0.1)
+          aes(x = x, y = y, col = gene), size = cex_pt * 2, alpha = alpha)
       } else {
         ge <- ge + geom_point(data = territory,
-          aes(x = x, y = y, shape = trial, col = gene), size = cex * 0.1)
+          aes(x = x, y = y, shape = trial, col = gene),
+          size = cex_pt * 2,
+          alpha = alpha)
       }
 
       ge <- ge +
         scale_color_gradientn(colors = rev(brewer.pal(11, "Spectral"))) +
         theme_classic() +
-        theme(#panel.background = element_rect(fill = "#474747"),
-          legend.title = element_text(size = cex),
+        theme(legend.title = element_text(size = cex),
           legend.text = element_text(size = cex),
           plot.margin = margin(1, 1, 1, 1, "cm")) +
         labs(col = type, title = names(gene_list)[i]) +
         guides(shape = guide_legend(override.aes = list(size = cex * 0.5)))
-
+      
+      if (with_background){
+        ge <- ge +
+          theme(panel.background = element_rect(
+            fill = rev(brewer.pal(11, "Spectral"))[1]))
+      }
       gene_list[[i]] <- ge
     }
     #--------------------------------------------------------------------------#
     # returning ggplot or ggplot list with ggarrange
     #--------------------------------------------------------------------------#
+    if (is.null(gene_list)) {
+      return(NULL)
+    } else {
+      gene_list <- gene_list[!sapply(gene_list, is.null)]
+    }
+
     if (length(gene_list) == 1) {
       gene_list <- gene_list[[1L]]
     } else {
