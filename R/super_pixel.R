7ec335fceff02698657fd7c921bad28786330000
patrickCNMartin <pcnmartin@gmail.com>
2023-03-23 16:50:27 -0700

    Feature(bubble):intial idea for bubble packing algo - used to select inital starting points for super pixels

--- R/super_pixel.R
+++ R/super_pixel.R
@@ -10,47 +10,33 @@
 #' @return barcodes of starting coordinates
 #' @importFrom future.apply future_apply
 select_initial_indices <- function(coordinates,
-    k = 500) {
-    #---------------------------------------------------------------------#
-    # First we get mid point of each box 
-    #---------------------------------------------------------------------#
-    box_size <- sqrt(k)
-    x <- seq(min(coordinates$x),
-        max(coordinates$x),
-        length.out = (2 * floor(box_size)) - 1)
-    x <- x[seq(2, length(x), by = 2)]
-    y <- seq(min(coordinates$y),
-        max(coordinates$y),
-        length.out = (2 * floor(box_size)) - 1)
-    y <- y[seq(2, length(y), by = 2)]
-    grid <- cbind(rep(x, each = length(y)),
-        rep(y, times = length(x)),
-        seq(1, length(y) * length(x)))
-    grid <- future_apply(coordinates,
-        MARGIN = 1,
-        closet_grid_point,
-        grid)
-    
-    grid <- do.call("rbind", grid)
-    return(grid)
+    n_centers = 500,
+    max_iter = 500) {
+    #-------------------------------------------------------------------------#
+    # intialise background grid, active grod and get nearest neighbors
+    # we assume that you will have at least 4 inital points
+    #-------------------------------------------------------------------------#
+    background_grid <- rep(0, n_centers)
+    knn <- RANN::nn2(coordinates[, c("x", "y")], k = nrow(coordinates))
+    active <- seq_len(nrow(coordinates))
+    radius <- srqt(max(knn$nn.dist)) 
+    #-------------------------------------------------------------------------#
+    # iterated a reduced space until you get the desired numbers of circles  
+    #-------------------------------------------------------------------------#
+    iter <- TRUE
+    while (iter) {
+        nn_index <- knn$nn.idx
+        nn_dist <- knn$nn.dist
+        # bubble packing
+    }
 }
 
-#' closest grid point
-#' @param grid grid matrix containing coordinate paiur of grid point centers
-#' @param coordinates data frame with spatial indices
-#' @return closest coordinates to grid mid point
-closet_grid_point <- function(coordinates, grid) {
-    distance <- sqrt(((grid[, 1] + as.numeric(coordinates[2]))^2 +
-        (grid[, 2] + as.numeric(coordinates[3]))^2))
-    distance <- which(distance == min(distance))
-    coordinates <- data.frame("barcodes" = coordinates[1],
-        "xcoord" = coordinates[2],
-        "ycoord" = coordinates[3],
-        "x_cent" = grid[distance, 1],
-        "y_cent" = grid[distance, 2],
-        "center" = grid[distance, 3])
-    return(coordinates)
-}
+
+
+
+
+
+
 
 # #' @importFrom imager imappend imsplit nPix spectrum
 # #' @importFrom purrr map_dbl
